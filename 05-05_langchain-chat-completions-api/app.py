92961dc62378e8f5376d095355542adf4f386737
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2023-07-28 13:42:11 +0900

    Add 05-05

---
+++ 05-05_langchain-chat-completions-api/app.py
@@ -0,0 +1,34 @@
+import os
+
+import streamlit as st
+from dotenv import load_dotenv
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage
+
+load_dotenv()
+
+
+st.title("langchain-book-streamlit-app")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        chat = ChatOpenAI(
+            model_name=os.environ["OPENAI_API_MODEL"],
+            temperature=os.environ["OPENAI_API_TEMPERATURE"],
+        )
+        messages = [HumanMessage(content=prompt)]
+        response = chat(messages)
+        st.markdown(response.content)
+
+    st.session_state.messages.append({"role": "assistant", "content": response})
