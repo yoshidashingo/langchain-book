39114022dd99bb6c1297a40ac73fed61e8e7838e
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2023-07-28 14:04:58 +0900

    Add 05-07

---
+++ 05-07_memory/app.py
@@ -0,0 +1,59 @@
+import os
+
+import streamlit as st
+from dotenv import load_dotenv
+from langchain.agents import AgentType, initialize_agent, load_tools
+from langchain.callbacks import StreamlitCallbackHandler
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import MessagesPlaceholder
+
+load_dotenv()
+
+
+def create_agent():
+    chat = ChatOpenAI(
+        model_name=os.environ["OPENAI_API_MODEL"],
+        temperature=os.environ["OPENAI_API_TEMPERATURE"],
+        streaming=True,
+    )
+
+    agent_kwargs = {
+        "extra_prompt_messages": [MessagesPlaceholder(variable_name="memory")],
+    }
+    memory = ConversationBufferMemory(memory_key="memory", return_messages=True)
+
+    tools = load_tools(["ddg-search", "wikipedia"])
+    return initialize_agent(
+        tools,
+        chat,
+        agent=AgentType.OPENAI_FUNCTIONS,
+        agent_kwargs=agent_kwargs,
+        memory=memory,
+    )
+
+
+if "agent" not in st.session_state:
+    st.session_state.agent = create_agent()
+
+
+st.title("langchain-book-streamlit-app")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        callback = StreamlitCallbackHandler(st.container())
+        response = st.session_state.agent.run(prompt, callbacks=[callback])
+        st.markdown(response)
+
+    st.session_state.messages.append({"role": "assistant", "content": response})
