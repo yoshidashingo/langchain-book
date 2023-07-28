a91d6b905fa46caa05a9e88845e625011b3060a4
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2023-07-28 13:56:13 +0900

    Add 05-06

---
+++ 05-06_agent/app.py
@@ -0,0 +1,43 @@
+import os
+
+import streamlit as st
+from dotenv import load_dotenv
+from langchain.agents import AgentType, initialize_agent, load_tools
+from langchain.callbacks import StreamlitCallbackHandler
+from langchain.chat_models import ChatOpenAI
+
+load_dotenv()
+
+
+def create_agent():
+    chat = ChatOpenAI(
+        model_name=os.environ["OPENAI_API_MODEL"],
+        temperature=os.environ["OPENAI_API_TEMPERATURE"],
+        streaming=True,
+    )
+
+    tools = load_tools(["ddg-search", "wikipedia"])
+    return initialize_agent(tools, chat, agent=AgentType.OPENAI_FUNCTIONS)
+
+
+st.title("langchain-book-streamlit-app")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        callback = StreamlitCallbackHandler(st.container())
+        agent = create_agent()
+        response = agent.run(prompt, callbacks=[callback])
+        st.markdown(response)
+
+    st.session_state.messages.append({"role": "assistant", "content": response})
