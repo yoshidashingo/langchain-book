a1b44cda6b1bc7e8177fe2d1197f4b3f9b2c7251
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2023-07-31 21:20:28 +0900

    Add 05-04

---
+++ 05-04_input/app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("langchain-book-streamlit-app")
+
+if prompt := st.chat_input("What is up?"):
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        response = "こんにちは"
+        st.markdown(response)
