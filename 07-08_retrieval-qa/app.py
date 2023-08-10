1d767fda826504d20b0e432782cb96b463174fb3
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2023-08-10 13:41:46 +0900

    Add 07-08

---
+++ 07-08_retrieval-qa/app.py
@@ -0,0 +1,137 @@
+import json
+import logging
+import os
+import time
+from typing import Any
+
+import openai
+from dotenv import load_dotenv
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import LLMResult
+from slack_bolt import App
+from slack_bolt.adapter.aws_lambda import SlackRequestHandler
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+from add_document import initialize_vectorstore
+
+CHAT_UPDATE_INTERVAL_SEC = 1
+
+load_dotenv()
+
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+# ボットトークンとソケットモードハンドラーを使ってアプリを初期化します
+app = App(
+    signing_secret=os.environ["SLACK_SIGNING_SECRET"],
+    token=os.environ["SLACK_BOT_TOKEN"],
+    process_before_response=True,
+)
+
+# ログ
+SlackRequestHandler.clear_all_log_handlers()
+logging.basicConfig(
+    format="%(asctime)s [%(levelname)s] %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+
+class SlackStreamingCallbackHandler(BaseCallbackHandler):
+    last_send_time = time.time()
+    message = ""
+
+    def __init__(self, channel, ts, id_ts):
+        self.channel = channel
+        self.ts = ts
+        self.id_ts = id_ts
+        self.interval = CHAT_UPDATE_INTERVAL_SEC
+        # 投稿を更新した累計回数カウンタ
+        self.update_count = 0
+
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        self.message += token
+
+        now = time.time()
+        if now - self.last_send_time > self.interval:
+            app.client.chat_update(
+                channel=self.channel, ts=self.ts, text=f"{self.message}\n\nTyping..."
+            )
+            self.last_send_time = now
+            self.update_count += 1
+
+            # update_countが現在の更新間隔X10より多くなるたびに更新間隔を2倍にする
+            if self.update_count / 10 > self.interval:
+                self.interval = self.interval * 2
+
+    def on_llm_end(self, response: LLMResult, **kwargs: Any) -> Any:
+        message_context = "OpenAI APIで生成される情報は不正確または不適切な場合がありますが、当社の見解を述べるものではありません。"
+        message_blocks = (
+            '''[
+            {"type": "section", "text": {"type": "mrkdwn", "text": "'''
+            + self.message
+            + '''"}},
+            {"type": "divider"},
+            {"type": "context","elements": [{"type": "mrkdwn","text": "'''
+            + message_context
+            + """"}]}
+            ]
+        """
+        )
+        app.client.chat_update(channel=self.channel, ts=self.ts, blocks=message_blocks)
+
+
+# @app.event("app_mention")
+def handle_mention(event, say):
+    channel = event["channel"]
+    thread_ts = event["ts"]
+    message = event["text"]
+
+    # 投稿の先頭(=Momentoキー)を示す：初回はevent["ts"],2回目以降はevent["thread_ts"]
+    id_ts = event["ts"]
+    if "thread_ts" in event:
+        id_ts = event["thread_ts"]
+
+    result = say("\n\nTyping...", thread_ts=thread_ts)
+    ts = result["ts"]
+
+    vectorstore = initialize_vectorstore()
+
+    callback = SlackStreamingCallbackHandler(channel=channel, ts=ts, id_ts=id_ts)
+    llm = ChatOpenAI(
+        model_name=os.environ["OPENAI_API_MODEL"],
+        temperature=os.environ["OPENAI_API_TEMPERATURE"],
+        streaming=True,
+        callbacks=[callback],
+    )
+
+    qa_chain = RetrievalQA.from_llm(llm=llm, retriever=vectorstore.as_retriever())
+
+    qa_chain.run(message)
+
+
+def just_ack(ack):
+    ack()
+
+
+app.event("app_mention")(ack=just_ack, lazy=[handle_mention])
+
+
+# アプリを起動します
+if __name__ == "__main__":
+    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
+
+
+def handler(event, context):
+    logger.info("handler called")
+    header = event["headers"]
+    logger.info(json.dumps(header))
+
+    if "x-slack-retry-num" in header:
+        logger.info("SKIP > x-slack-retry-num: " + header["x-slack-retry-num"])
+        return 200
+
+    # AWS Lambda 環境のリクエスト情報を app が処理できるよう変換してくれるアダプター
+    slack_handler = SlackRequestHandler(app=app)
+    # 応答はそのまま AWS Lambda の戻り値として返せます
+    return slack_handler.handle(event, context)
