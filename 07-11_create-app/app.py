16af3218a933fcd1cb5484ddca76bf1b18f93bcb
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2023-08-28 21:38:33 +0900

    Add 07-11

---
+++ 07-11_create-app/app.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+load_dotenv()
+
+# ボットトークンとソケットモードハンドラーを使ってアプリを初期化します
+app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+
+# アプリを起動します
+if __name__ == "__main__":
+    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
