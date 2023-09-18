a5c347c606e3fd561d5ff6c4af3dfa4a475dde85
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2023-09-18 15:34:09 +0900

    Update 07-10

--- 07-10_call-openai-api/app.py
+++ 07-10_call-openai-api/app.py
@@ -1,15 +1,12 @@
 import os
 import re
 
-import openai
 from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 load_dotenv()
-
-openai.api_key = os.environ["OPENAI_API_KEY"]
 
 # ボットトークンを使ってアプリを初期化します
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
