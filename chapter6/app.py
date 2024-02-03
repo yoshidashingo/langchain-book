1a5d3635788c8f1c1bbec3de1da81d09ef57342c
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2024-02-03 12:35:39 +0900

    Update chapter6

--- chapter6/app.py
+++ chapter6/app.py
@@ -2,61 +2,56 @@ import os
 
 import streamlit as st
 from dotenv import load_dotenv
-from langchain.agents import AgentType, initialize_agent, load_tools
-from langchain.callbacks import StreamlitCallbackHandler
-from langchain.chat_models import ChatOpenAI
+from langchain import hub
+from langchain.agents import AgentExecutor, create_openai_tools_agent, load_tools
 from langchain.memory import ConversationBufferMemory
-from langchain.prompts import MessagesPlaceholder
+from langchain_community.callbacks import StreamlitCallbackHandler
+from langchain_community.chat_message_histories import StreamlitChatMessageHistory
+from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
 
-def create_agent_chain():
+def create_agent_chain(history):
     chat = ChatOpenAI(
         model_name=os.environ["OPENAI_API_MODEL"],
         temperature=os.environ["OPENAI_API_TEMPERATURE"],
-        streaming=True,
     )
-
-    agent_kwargs = {
-        "extra_prompt_messages": [MessagesPlaceholder(variable_name="memory")],
-    }
-    memory = ConversationBufferMemory(memory_key="memory", return_messages=True)
 
     tools = load_tools(["ddg-search", "wikipedia"])
-    return initialize_agent(
-        tools,
-        chat,
-        agent=AgentType.OPENAI_FUNCTIONS,
-        agent_kwargs=agent_kwargs,
-        memory=memory,
+
+    prompt = hub.pull("hwchase17/openai-tools-agent")
+
+    memory = ConversationBufferMemory(
+        chat_memory=history, memory_key="chat_history", return_messages=True
     )
 
-
-if "agent_chain" not in st.session_state:
-    st.session_state.agent_chain = create_agent_chain()
+    agent = create_openai_tools_agent(chat, tools, prompt)
+    return AgentExecutor(agent=agent, tools=tools, memory=memory)
 
 
 st.title("langchain-streamlit-app")
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+# StreamlitChatMessageHistoryを使用することで、
+# st.session_stateを使った会話履歴の管理を自前で実装する必要がなくなりました。
+history = StreamlitChatMessageHistory()
 
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+for message in history.messages:
+    st.chat_message(message.type).write(message.content)
 
 prompt = st.chat_input("What is up?")
 
 if prompt:
-    st.session_state.messages.append({"role": "user", "content": prompt})
-
     with st.chat_message("user"):
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
         callback = StreamlitCallbackHandler(st.container())
-        response = st.session_state.agent_chain.run(prompt, callbacks=[callback])
-        st.markdown(response)
 
-    st.session_state.messages.append({"role": "assistant", "content": response})
+        agent_chain = create_agent_chain(history)
+        response = agent_chain.invoke(
+            {"input": prompt},
+            {"callbacks": [callback]},
+        )
+
+        st.markdown(response["output"])
